64e06ac50dc91380523977255ba98b4ae1f7a5c4
moaalaa <moaalaa16@gmail.com>
2019-07-22 11:55:02 +0200

    Crafting Basic Structure

---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+
+mix.sass('src/css/app.scss', 'dist/css/');
+
+// mix.js('src/js/app.js', 'dist/js/');
+
+mix.sourceMaps(); // Enable sourcemaps
+
+// mix.version(); // Enable versioning.
+
