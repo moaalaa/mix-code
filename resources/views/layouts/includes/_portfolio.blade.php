6ca8ba350a7eb99a8a0309b02b37489c91b1c82e
hamza <hamza_omar@live.com>
2020-10-23 23:56:22 +0200

    fix some design

--- resources/views/layouts/includes/_portfolio.blade.php
+++ resources/views/layouts/includes/_portfolio.blade.php
@@ -10,7 +10,7 @@
 
         <!-- app section -->
  @foreach ($clients as $client)
- <div class="col-md-6 col-lg-4"  >
+ <div class="col-md-6 col-lg-4 col-sm-12"  >
  <div class="card portfolio-card " >
   <img class="card-img-top  portfolio-img" src="{{  $client->mainMediaUrl() }} " alt="Card image" style="width:100%">
   <div class="card-body text-center">
